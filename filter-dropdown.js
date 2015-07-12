15b82abf14fdc7328a5bb37280d6c3448ba6109b
tvitovic <uros.titovic@gmail.com>
2015-07-12 16:05:01 +0200

    minor bugfix

--- filter-dropdown.js
+++
@@ -1,6 +0,0 @@
-function showElement(id) {
-    document.getElementById(id).style.visibility = "visible";
-  }
-  function hideElement(id) {
-    document.getElementById(id).style.visibility = "hidden";
-  }
