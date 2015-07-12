640b3ffb858cd6176956bc9ad82ad2d08d99b1ba
tvitovic <uros.titovic@gmail.com>
2015-07-12 14:47:39 +0200

    filter and dropdown

---
+++ filter-dropdown.js
@@ -0,0 +1,6 @@
+function showElement(id) {
+    document.getElementById(id).style.visibility = "visible";
+  }
+  function hideElement(id) {
+    document.getElementById(id).style.visibility = "hidden";
+  }
